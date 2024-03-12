e94300658503b66a1c47eea4d823971efb6c84ed
alessonjrt <alesson.junior@outlook.com>
2024-03-12 18:09:47 -0300

    Abstracts classes

---
+++ lib/domain/business/dao_workflow.dart
@@ -0,0 +1,11 @@
+import 'package:hyrule/domain/models/entry.dart';
+
+abstract class DaoWorkflow {
+  Future<List<Entry>> getSavedEntries();
+
+  Future<void> saveEntry({required Entry entry});
+
+  Future<void> deleteEntry({required Entry entry});
+
+
+}
